7064131f074ff9e20ed0ec88c284e362eec60c3e
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-08-16 14:05:45 -0300

    Initital commit

---
+++ src/components/Home/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 30px;
+  position: fixed;
+
+  width: 100%;
+  height: 100%;
+
+  h1 {
+    color: #0000ff;
+  }
+`;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Home from '../../components/Home';
+
+const Region: React.FC = () => {
+  return <Home />;
+};
+
+export default Region;
