aa2fedf5c687da89941d1d6b7435ef258fc55d7d
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-08-26 18:06:24 -0300

    Changing URL locations for Django directories

--- postbuild.sh
+++ postbuild.sh
@@ -33,3 +33,6 @@ find $folder_name"/index.html" -type f | xargs sed -i "s/\/static\/js/{{ STATIC_
 find $folder_name"/index.html" -type f | xargs sed -i "s/\/static\/css/{{ STATIC_URL }}$folder_name\/static\/css/g"
 find $folder_name"/index.html" -type f | xargs sed -i "s/images\/favicons/{{ STATIC_URL }}$folder_name\/images\/favicons/g"
 find $folder_name"/index.html" -type f | xargs sed -i "s/http:\/\/localhost:3000/{{ STATIC_URL }}$folder_name\/images/g"
+
+find "$folder_name/static/js/" -name "*.js" -type f -exec sed -i "s/static\/media/static\/$folder_name\/static\/media/g" {} \;
+find "$folder_name/static/js/" -name "*.map" -type f -exec sed -i "s/static\/media/static\/$folder_name\/static\/media/g" {} \;
