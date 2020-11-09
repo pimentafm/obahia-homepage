fa2a3d3202d51488e93d29a6b87157c7fe0b1346
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-11-09 16:45:56 -0300

    Changing development date

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -64,7 +64,7 @@ const Home: React.FC = () => {
   };
 
   useEffect(() => {
-    ReactGA.initialize('UA-181577909-1');
+    ReactGA.initialize('UA-182410588-2');
     ReactGA.pageview('/');
 
     showNotificationModal();
@@ -266,7 +266,7 @@ const Home: React.FC = () => {
         </h3>
 
         <h3 style={{ textAlign: 'center' }}>
-          Esta versão foi atualizada em 27/10/2020.
+          Esta versão foi atualizada em 09/11/2020.
         </h3>
       </Modal>
       <ToastContainer
