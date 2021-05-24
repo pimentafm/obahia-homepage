60c6ab3800d66582778a68c44c2f6eba7100320e
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2021-05-24 14:18:14 -0300

    Create new pages

---
+++ src/pages/Governance/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Governance: React.FC = () => {
+  return (
+    <Container>
+      <h1>Governança</h1>
+    </Container>
+  );
+};
+
+export default Governance;
